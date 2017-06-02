e5a1b15a4fffa5c27c6f87a22a3b824cc2133737
wildpow <element1028@gmail.com>
2017-06-02 08:29:56 -0700

    added if incase index is nil

--- remove_ext.rb
+++ remove_ext.rb
@@ -1,11 +1,17 @@
+
+
 def remove_ext
   file = File.open('test.txt','r')
   new_file = File.new('new.txt', 'w')
   while line = file.gets do
     movie_title = line.chomp.to_s
     index = movie_title.index('.')
-    movie_title.slice!(index,8)
-    puts movie_title
+    if index != nil
+      movie_title.slice!(index,8)
+      puts movie_title
+    else
+      puts movie_title
+    end
   end
 end
 remove_ext
