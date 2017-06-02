51120f6c651782c298a26f804eecbf5086aa5baa
wildpow <element1028@gmail.com>
2017-06-02 09:04:28 -0700

    now removes just file extentions

--- remove_ext.rb
+++ remove_ext.rb
@@ -1,17 +1,28 @@
 
 
+
 def remove_ext
+  
+
   file = File.open('test.txt','r')
-  new_file = File.new('new.txt', 'w')
+# new_file = File.new('new.txt', 'w')
   while line = file.gets do
     movie_title = line.chomp.to_s
-    index = movie_title.index('.')
-    if index != nil
-      movie_title.slice!(index,8)
-      puts movie_title
-    else
-      puts movie_title
-    end
+
+      new_name = File.basename(movie_title,File.extname(movie_title))
+      puts new_name
+
   end
 end
+
+
+#    index = movie_title.index('.')
+#    if index != nil
+#      movie_title.slice!(index,8)
+#      puts movie_title
+#    else
+#      puts movie_title
+#    end
+#  end
+#end
 remove_ext
